cdc8c331e4f6fff9248a9364b9b9ea418b26732b
rajashree112 <107317983+rajashree112@users.noreply.github.com>
2023-02-05 13:30:47 +0530

    Create Validate Binary Search Tree.c

---
+++ Validate Binary Search Tree.c
@@ -0,0 +1,11 @@
+bool checkTree(struct TreeNode * root, long min, long max)
+{
+    if(!root) return true;
+    if(root->val <= min || root->val >= max) return false;
+    return checkTree(root->left, min, root->val) && checkTree(root->right, root->val, max);
+}
+
+bool isValidBST(struct TreeNode* root){
+    return checkTree(root,LONG_MIN, LONG_MAX);
+
+}
